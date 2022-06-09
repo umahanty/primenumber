c90c0fdee1ea38a1c3f9843015615f92e0843ccb
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-09 09:13:38 +0530

    primenumberornot

---
+++ primenumber.sh
@@ -0,0 +1,13 @@
+#Write a program that takes a input and determines if the number is a prime.
+echo -e "Enter Number: \c"
+read n
+for((i=2; i<=$n/2; i++))
+do
+ ans=$(( n%i ))
+ if [ $ans -eq 0 ]
+ then
+ echo "$n is not a prime number"
+ exit 0
+fi
+done
+echo "$n is a prime number"
